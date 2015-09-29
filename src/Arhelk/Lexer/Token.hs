5664d68da0feddbd5cf5a601f7a787e7eb59e964
Anton Gushcha <ncrashed@gmail.com>
2015-09-29 17:48:11 +0300

    Add direct speech token (not yet in lexer) and some docs

--- src/Arhelk/Lexer/Token.hs
+++ src/Arhelk/Lexer/Token.hs
@@ -6,18 +6,33 @@ import Data.Monoid
 import Data.Text as T
 import TextShow 
 
+-- | Kinds of tokens in generic lexer
+-- Some tokens could be not used in particular languages.
 data Token =
+  -- | Sequence on non space and non punctuational symbols
     Word Text 
+  -- | End of sentence. Example '.'
   | EndSentence
+  -- | Question sign, could also mark end of sentence. Example '?'
   | QuestionMark
+  -- | Exclamation sign, could also mark end of sentence. Example '!'
   | ExclamationMark
+  -- | Special sign that marks motive inclination. Example '՛'
   | MotiveMark
+  -- | Special sign that marks dependent clause. Example '՝'
   | DependentMark
+  -- | Comma sign. Example ','
   | Comma
+  -- | Semicolon sign. Example ';'
   | Semicolon
+  -- | Citation sign. Example ':'
   | Citation
+  -- | Dash sign. Example '—'
+  | Dash
+  -- | Quotation region. Example '‘’'
   | Quotation [Token]
-  -- | TODO: direct speech
+  -- | Direct speech region. Example '—'. 
+  | DirectSpeech [Token]
   deriving Show
 
 instance TextShow Token where 
@@ -27,8 +42,9 @@ instance TextShow Token where
     QuestionMark -> "Q"
     ExclamationMark -> "EX"
     MotiveMark -> "M"
-    DependentMark -> "D"
+    DependentMark -> "DE"
     Comma -> "CO"
     Semicolon -> "S"
     Citation -> "CI"
+    Dash -> "DA"
     Quotation t -> "QS\n" <> unlinesB (showb <$> t) <> "QE"

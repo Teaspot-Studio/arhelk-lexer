6ae5e69aa26be2c256f3e1063c5139d04794747b
Anton Gushcha <ncrashed@gmail.com>
2015-09-29 17:06:15 +0300

    Ported lexer grammar to language spec

--- app/Main.hs
+++ app/Main.hs
@@ -12,8 +12,8 @@ import Arhelk.Lexer.Grammar
 import Arhelk.Lexer.Language
 import TextShow 
 
-parseInput :: Text -> Either ParseError [Token]
-parseInput input = parse arhelkLexer "(unknown)" input
+parseInput :: LexerLanguage -> Text -> Either ParseError [Token]
+parseInput l  input = parse (arhelkLexer l) "(unknown)" input
 
 bibleArm :: Text
 bibleArm = "Ի սկզբանէ Աստուած ստեղծեց երկինքն ու երկիրը։ Երկիրն անձեւ ու անկազմ էր, խաւար էր տիրում անհունի վրայ, եւ Աստծու հոգին շրջում էր ջրերի վրայ։ Եւ Աստուած ասաց. «Թող լոյս լինի»։ Եւ լոյս եղաւ։ Աստուած տեսաւ, որ լոյսը լաւ է, եւ Աստուած լոյսը բաժանեց խաւարից։ Աստուած լոյսը կոչեց ցերեկ, իսկ խաւարը կոչեց գիշեր։ Եւ եղաւ երեկոյ, եւ եղաւ առաւօտ՝ օր առաջին։ Աստուած ասաց. «Թող տարածութիւն առաջանայ ջրերի միջեւ, եւ ջրերը թող բաժանուեն ջրերից»։ Եւ եղաւ այդպէս։ Աստուած ստեղծեց տարածութիւնը, որով Աստուած տարածութեան ներքեւում եղած ջրերը անջրպետեց տարածութեան վրայ եղած ջրերից։ ։։"
@@ -21,12 +21,12 @@ bibleArm = "Ի սկզբանէ Աստուած ստեղծեց երկինքն ու 
 bibleEsp :: Text 
 bibleEsp = "En la komenco Dio kreis la cxielon kaj la teron. Kaj la tero estis senforma kaj dezerta, kaj mallumo estis super la abismo; kaj la spirito de Dio sxvebis super la akvo. Kaj Dio diris: Estu lumo; kaj farigxis lumo. Kaj Dio vidis la lumon, ke gxi estas bona; kaj Dio apartigis la lumon de la mallumo. Kaj Dio nomis la lumon Tago, kaj la mallumon Li nomis Nokto. Kaj estis vespero, kaj estis mateno, unu tago."
 
-armLang :: LexerLanguage 
-armLang = defaultLexer {
+armenianLang :: LexerLanguage 
+armenianLang = defaultLexer {
     lexerPunctuation = [
         char '։' >> return EndSentence
       , char ',' >> return Comma
-      , char '․' >> return Citation
+      , (char '․' <|> char '.') >> return Citation
       , char '՝' >> return DependentMark
       ] 
       ++ inwords
@@ -44,7 +44,7 @@ armLang = defaultLexer {
 
 main :: IO ()
 main = do
-  let es = parseInput bibleArm
+  let es = parseInput armenianLang bibleArm
   case es of 
     Left err -> print err
     Right res -> putStrLn $ T.unpack $ T.unlines $ showt <$> res
--- src/Arhelk/Lexer/Grammar.hs
+++ src/Arhelk/Lexer/Grammar.hs
@@ -2,6 +2,7 @@ module Arhelk.Lexer.Grammar(
     arhelkLexer
   ) where 
 
+import Arhelk.Lexer.Language
 import Arhelk.Lexer.Token 
 import Control.Monad 
 import Data.Monoid
@@ -9,31 +10,41 @@ import Data.Text as T
 import Text.Parsec 
 import Text.Parsec.Text 
 
-arhelkLexer :: Parser [Token]
-arhelkLexer = Prelude.concat <$> someToken `sepBy` spaces
+arhelkLexer :: LexerLanguage -> Parser [Token]
+arhelkLexer lang@(LexerLanguage {..}) = Prelude.concat <$> someToken `sepBy` spaces
   where 
     someToken :: Parser [Token]
-    someToken = (singleton <$> endSent) 
-      <|> (singleton <$> question)
-      <|> (singleton <$> quotation)
-      <|> wordWith [endSent, question, quotation]
-
-    singleton t = [t]
+    someToken = choice $ (many1 <$> lexerPunctuation) ++
+      (fmap (aglutted $ choice lexerPunctuation) [single <$> quotation, wordWith lexerInwordMarks])
 
     wordWith :: [Parser Token] -> Parser [Token]
     wordWith ps = do 
       w <- many (choice $ ps++[word])
-      return $ concatTokens w 
+      return $ supplantTokens w
 
-    concatTokens [] = []
-    concatTokens [t] = [t]
-    concatTokens ((Word t1):(Word t2):xs) = (Word $ t1 <> t2):(concatTokens xs)
-    concatTokens ((Word t1):t2:(Word t3):xs) = (Word $ t1 <> t3):t2:(concatTokens xs)
-    concatTokens (t1:t2:xs) = t1:t2:(concatTokens xs)
+    -- | Supplant non-word tokens within word tokens
+    supplantTokens :: [Token] -> [Token]
+    supplantTokens [] = []
+    supplantTokens [t] = [t]
+    supplantTokens ((Word t1):(Word t2):xs) = (Word $ t1 <> t2):(supplantTokens xs)
+    supplantTokens ((Word t1):t2:(Word t3):xs) = (Word $ t1 <> t3):t2:(supplantTokens xs)
+    supplantTokens (t1:t2:xs) = t1:t2:(supplantTokens xs)
 
     word :: Parser Token
-    word = Word . T.pack <$> many1 (noneOf " ։՞«»\n")
-    spaces = oneOf " \n\t\r" >> return Space
-    endSent = char '։' >> return EndSentence 
-    question = char '՞' >> return QuestionMark
-    quotation = Quotation <$> between (char '«') (char '»') arhelkLexer
+    word = fmap (Word . T.pack) $ many1 $ do 
+      let quotationChars = (\(b,e) -> (b <|> e) >> return (Quotation [])) <$> lexerQuotation
+      notFollowedBy (choice $ spaces : lexerPunctuation ++ quotationChars)
+      anyChar
+
+    spaces = oneOf (T.unpack lexerSpaces) >> return Space
+
+    quotation = Quotation <$> choice (mkQuot <$> lexerQuotation)
+      where mkQuot (b, e) = between b e $ arhelkLexer lang
+
+    -- | Parser with followed tokens without spaces
+    aglutted aglutp p = do 
+      pres <- p 
+      gluts <- many aglutp 
+      return $ pres ++ gluts
+
+    single t = [t]
